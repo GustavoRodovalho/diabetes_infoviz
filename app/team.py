68ad160e90884e3603f2657851d2cf79406c2ba7
Gustavo Rodovalho <gustawhat@gmail.com>
2023-07-05 21:34:54 -0300

    updated team photos

--- app/team.py
+++ app/team.py
@@ -7,15 +7,15 @@ def team():
 
     with col1:
         st.header("Gustavo Rodovalho")
-        st.image("https://static.streamlit.io/examples/cat.jpg")
+        st.image("https://media.licdn.com/dms/image/C4D03AQHIDz4XFBs-Ow/profile-displayphoto-shrink_200_200/0/1624586584999?e=1694044800&v=beta&t=Zn9xaNowsEC0EeNDTzy97tKg8MA-eGelgPSI6OfpoHk")
         st.caption("Mestrando em Engenharia Mineral - PPGEMin.")
 
     with col2:
         st.header("Rafael Ferrari")
-        st.image("https://static.streamlit.io/examples/dog.jpg")
+        st.image("https://media.licdn.com/dms/image/C4E03AQFanoFtnedCxw/profile-displayphoto-shrink_200_200/0/1625500020503?e=1694044800&v=beta&t=VYcNBFq4d0342DAhWv4h3ROygAdN2PxSKmiI0CDKlZU")
         st.caption("Mestrando em Ciência de Dados - MECAI.")
 
     with col3:
         st.header("Renata Cecconi")
-        st.image("https://static.streamlit.io/examples/owl.jpg")
+        st.image("https://media.licdn.com/dms/image/D4D03AQEJzHChlK1ydA/profile-displayphoto-shrink_200_200/0/1669078225612?e=1694044800&v=beta&t=5xeAhTTnpVqhc4dD3gruUJuhPOBQDqJIGsgqB50rXh4")
         st.caption("Mestranda em Ciência de Dados - MECAI.")
